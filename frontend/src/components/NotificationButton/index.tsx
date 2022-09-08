c1cc17df16f8bab678bee6c7c2f2fd8c2b12adac
D C <dc3@mailbox.org>
2022-09-07 21:44:23 -0300

    Primeiro componente React

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/dsmeta-btn.svg'
+import './styles.css'
+function NotificationButton() {
+  return (
+    <>
+    <div className="red-btn">
+      <img src={icon} alt="Notificação" />
+    </div>
+    </>               
+    )
+}
+
+export default NotificationButton
